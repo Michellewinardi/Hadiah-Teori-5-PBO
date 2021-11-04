7018e8dbade0b31effbe58c9275adf8e14a1f1d8
Michellewinardi <90232337+Michellewinardi@users.noreply.github.com>
2021-11-04 11:52:06 +0800

    Add files via upload

---
+++ volbola.java
@@ -0,0 +1,11 @@
+// Michelle- 51019016
+
+import java.util.Scanner;
+
+public class volbola {
+	
+	void radius(int jari) {
+	System.out.println("Nilai Jari - jari Bola adalah: "+jari + " cm");
+	}
+	
+}
